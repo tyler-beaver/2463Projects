112ac3bf425563b9377ac1f662ce2d728067a27b
tbeaver23 <tbeaver23@yahoo.com>
2023-01-23 17:20:02 -0600

    updating code for circles

--- DrawingWithP5/ThreeColorCircles/sketch.js
+++ DrawingWithP5/ThreeColorCircles/sketch.js
@@ -25,17 +25,7 @@ function draw(){
     noStroke();
     circle(70,60,55); 
     pop();
-
-    // push();
-    // noStroke();
-    // colorMode(HSB, 255);
-    // let c = color(0, 126, 255);
-    // fill(c);
-    // rect(15, 20, 35, 60);
-    // let value = saturation(c); // Sets 'value' to 126
-    // fill(value);
-    // rect(50, 20, 35, 60);
-    // pop();
+    
 }
 
 
