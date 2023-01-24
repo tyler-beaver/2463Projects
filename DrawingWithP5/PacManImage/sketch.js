0ba4cd3c255fff87e5a6b25f998e30910bd0dcc8
tbeaver23 <tbeaver23@yahoo.com>
2023-01-23 20:53:01 -0600

    final update

--- DrawingWithP5/PacManImage/sketch.js
+++ DrawingWithP5/PacManImage/sketch.js
@@ -1,7 +1,37 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(200, 100);
 }
 
 function draw() {
-  background(220);
+  background(0);
+
+  push();
+  pacMan = createGraphics(width, height);
+  pacMan.fill(255,230,0);
+  pacMan.ellipse(55,50,70,70);
+  ctx = pacMan.canvas.getContext("2d");
+  ctx.clip();
+  pacMan.fill(0,0,0);
+  pacMan.triangle(55, 50, 0, 100, 0, 0);
+  image(pacMan,0,0);
+  pop();
+
+  push();
+  blinkY = createGraphics(width, height);
+  blinkY.fill(255,50,0);
+  blinkY.rect(110, 15, 70, 70, 60, 60, 0, 0);
+  ctxx = blinkY.canvas.getContext("2d");
+  ctxx.clip();
+  blinkY.fill(255,255,255);
+  blinkY.noStroke();
+  blinkY.ellipse(130, 50, 20, 20);
+  blinkY.fill(0,0,255);
+  blinkY.ellipse(130, 50, 13, 13);
+  blinkY.fill(255,255,255);
+  blinkY.ellipse(160, 50, 20, 20);
+  blinkY.fill(0,0,255);
+  blinkY.ellipse(160, 50, 13, 13);
+  image(blinkY,0,0);
+  pop();
+
 }
--- DrawingWithP5/RedStarGreenCircle/sketch.js
+++ DrawingWithP5/RedStarGreenCircle/sketch.js
@@ -1,7 +1,32 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(200, 200);
 }
 
 function draw() {
-  background(220);
+  background(0,12,102);
+
+  push();
+  stroke("white")
+  strokeWeight(3);
+  fill(0,102,0);
+  circle(100,100,100);
+  fill(255,0,0);
+  star(100, 100, 20, 50, 5);
+  pop();
 }
+
+function star(x, y, radius1, radius2, npoints) {
+  let angle = TWO_PI / npoints;
+  let halfAngle = angle / 2.0;
+  beginShape();
+  for (let a = .95; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius2;
+    let sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a + halfAngle) * radius1;
+    sy = y + sin(a + halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
+
