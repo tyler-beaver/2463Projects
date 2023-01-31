ed1cef1ae6a9a3b88b3319301bcaea777b223ed8
tbeaver23 <tbeaver23@yahoo.com>
2023-01-31 14:01:07 -0600

    update

--- SpriteAnimation/SpriteAnimation.js
+++ SpriteAnimation/SpriteAnimation.js
@@ -1,7 +1,23 @@
+let spelunkyGuy;
+let x = 200;
+let y = 200;
+let r = 0;
+
+function preload(){
+  spelunkyGuy = loadImage("assets/SpelunkyGuy.png");
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(700, 600);
+  imageMode(CENTER);
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(220);
+  translate(x, y)
+  rotate(r); 
+  //r += .1;
+  scale(0.5);
+  image(spelunkyGuy, 300, 250)
 }
